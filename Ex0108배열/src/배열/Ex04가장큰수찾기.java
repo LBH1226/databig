2f8853c23d298468e2270a6df01bc82b40a0b40c
LBH1226 <leebh9712@gmail.com>
2024-01-08 16:53:32 +0900

    upload

---
+++ Ex0108배열/src/배열/Ex04가장큰수찾기.java
@@ -0,0 +1,40 @@
+package 배열;
+
+import java.util.Random;
+
+public class Ex04가장큰수찾기 {
+
+	public static void main(String[] args) {
+
+		Random ran = new Random();
+		int[] array = new int[5];
+		
+	
+//		int num2 =0;
+		System.out.print("배열 안에 들어있는 값 : ");
+		for (int i = 0; i < array.length; i++) {
+			int num1 = ran.nextInt(9)+1;
+			array[i]=num1;
+			System.out.print(array[i]+" ");
+//				if(i==0) {
+//					num2=array[0];
+//				}
+//			
+//			if(num2<=array[i]) {
+//				num2=array[i];
+//			}
+		}
+		int num2 = array[0];
+		for(int i = 0; i < array.length; i++) {
+			if(num2<=array[i]) {
+				num2 = array[i];
+			}
+		}
+		System.out.println();
+		System.out.println(num2);
+		
+	
+		
+	}
+
+}
